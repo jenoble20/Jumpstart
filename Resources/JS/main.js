cc1051ed82b08683e6dd7c638b023fe3660411e2
jenoble <jenoble20@gmail.com>
2018-08-30 18:15:12 -0400

    Adds carousel. Images react to hovering. Whole page reacts to viewport size.

---
+++ Resources/JS/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".carousel").slick({
+    accessibility: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
