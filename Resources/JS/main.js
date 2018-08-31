624d24daa24fa22f884513f1055673cb8514ccd8
jenoble <jenoble20@gmail.com>
2018-08-30 20:28:29 -0400

    Mobile optimization complete. Floating button on carousel working correctly. Navbar animation in progress.

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
   $(".carousel").slick({
     accessibility: false,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 4000,
     arrows: false,
-    draggable: false,
+    draggable: true,
     pauseOnFocus: false,
     pauseOnHover: false
   });
+
+  $(window).scroll(function(){
+    var scrollDist = $(this).scrollTop();
+    //console.log(scrollDist);
+    if(scrollDist > 200){
+      var max = -74;
+      var navHeight = $(".navbar").height();
+      var navbar = $(".navbar");
+    }
+    if(scrollDist === 0){
+      $(navbar).animate({top: navHeight},150);
+    }
+  });
+
 });
